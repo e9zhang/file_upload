0347cec07804d9b14c2a41eedb255d69a6492aba
e9zhang <emm.zhang996@gmail.com>
2017-10-04 00:59:47 -0400

    remove unused lines

--- src/test/java/com/fileupload/FileUploadTests.java
+++ src/test/java/com/fileupload/FileUploadTests.java
@@ -7,13 +7,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,20 +23,12 @@ public class FileUploadTests {
     @Autowired
     private MockMvc mvc;
 
-    @MockBean
-    private FileUploadService fileUploadService;
-
     @Test
     public void shouldSaveUploadedFile() throws Exception {
         MockMultipartFile multipartFile = new MockMultipartFile("file", "test.txt",
                 "text/plain", "test file".getBytes());
-        FileUploadModel model = new FileUploadModel();
-        //model.setId("123");
-        model.setFile(multipartFile);
         this.mvc.perform(fileUpload("/api/upload").file(multipartFile))
                 .andExpect(status().isOk());
-
-        //then(this.fileUploadService).should().saveUploadedFile(model);
     }
 
 }
